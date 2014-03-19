aad49cc6073a82cbf56c0bd1bebf7ef37d566c4b
Mitchell Plamann <mitchell.plamann@gmail.com>
2014-03-19 02:51:41 -0400

    Fixed error where some LaTeX keybindings would make the user unable to type a capital C

--- language-specific/latex.el
+++ language-specific/latex.el
@@ -1,14 +1,14 @@
 (fset 'latex-code
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([92 99 111 100 101 123 125 left] 0 "%d")) arg)))
 
-;; (add-hook 'LaTeX-mode-hook (lambda ()
-;;                         (local-set-key "C-c i" 'latex-code)))
+(add-hook 'LaTeX-mode-hook (lambda ()
+                             (local-set-key (kbd "C-c i") 'latex-code)))
 
 (fset 'latex-text
    [?& ?\\ ?t ?e ?x ?t ?\{ ?\[ ?\] ?\} ?\\ ?\\ left left left left])
 
-;; (add-hook 'LaTeX-mode-hook (lambda ()
-;;                         (local-set-key "C-c t" 'latex-text)))
+(add-hook 'LaTeX-mode-hook (lambda ()
+                             (local-set-key (kbd "C-c t") 'latex-text)))
 
 (defun compile-latex-hook ()
   (shell-command-to-string
@@ -17,3 +17,7 @@
 (add-hook 'LaTeX-mode-hook
           (lambda ()
             (add-hook 'after-save-hook 'compile-latex-hook 't 't)))
+
+;;; This is a good idea.
+;; (defun type-c () (interactive) (insert "C"))
+;; (global-set-key (kbd "C-t") 'type-c)
